606e470b02effdc53c7e9120c96048cf9046527d
JayDeveloperInc <71883769+JayDeveloperInc@users.noreply.github.com>
2021-07-13 21:02:22 +0200

    These files are from our last repository with a few modifications

---
+++ ttt_unittest.py
@@ -0,0 +1,14 @@
+import unittest
+from tictactoe import *
+
+class tttTest(unittest.TestCase):
+    def test_insertLetter(self):
+        results = spaceIsFree(1)
+        self.assertFalse(results)
+
+    # space is free method testing
+    
+
+if __name__ == '__main__':
+    unittest.main() 
+#zxczxczxc
