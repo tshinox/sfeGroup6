669ac25898f74e8f63731b79d67254f7fad708d6
tshinox <62985607+tshinox@users.noreply.github.com>
2021-07-16 16:07:35 +0200

    Add files via upload

--- ATM_Simulation.py
+++ ATM_Simulation.py
@@ -1,14 +1,18 @@
 from os import system
 from datetime import date
 from time import sleep
-import random
+
+checking = [5]
+savings = [5]
 
 
 class Account:
-    def __init__(self, idNum, balance, dateCreated):
+    def __init__(self, idNum, balance, dateCreated, index):
         self.id = idNum
         self.balance = balance
         self.dateCreated = dateCreated
+        self.index = index
+        interest = 0
 
     def getId(self):
         return self.id
@@ -19,37 +23,46 @@ class Account:
     def getDateCreated(self):
         return self.DateCreated
 
-    def setAccount(self, Id, Balance):
-        self.id = Id
-        self.balance = Balance
+    def setAccount(self):
+        savings[self.index] = self.id
+        checking[self.index] = self.id
+        self.balance = self.balance
 
     def withdraw(self, amount):
         self.balance = self.balance - amount
         return self.balance
 
     def deposit(self, amount):
-        self.balance = self.balance + amount
-        return self.balance
+        if self.balance < 0:
+            checking[self.index] = CheckingAccount(self.balance)
+            self.balance = amount + checking[self.index].getOverdraft()
+            return self.balance
+        else:
+            savings[self.index] = SavingsAccount(12)
+            self.balance += savings[self.index].getMonthlyInterest()
+            self.balance = amount + self.balance
+            return self.balance
 
 
 class CheckingAccount(Account):
     def __init__(self, overdraft):
         self.overdraft = overdraft
 
-    def overdraft(self):
+    def getOverdraft(self):
         return self.overdraft
 
 
 class SavingsAccount(Account):
-    def __init__(self, annualInterestRate, monthlyInterestRate):
+    def __init__(self, annualInterestRate):
         self.annualInterestRate = annualInterestRate
-        self.monthlyInterestRate = monthlyInterestRate
+        self.monthlyInterestRate = 0
 
     def getMonthlyInterestRate(self):
         self.monthlyInterestRate = (self.annualInterestRate / 100.0) / 12.0
         return self.monthlyInterestRate
 
     def getMonthlyInterest(self):
+        self.getMonthlyInterestRate()
         return self.balance * self.monthlyInterestRate
 
 
@@ -68,17 +81,16 @@ def menu():
     print("1. Check the balance\n2. Withdraw\n3. Deposit\n4. Exit")
 
 
-savings = []
-checking = []
 option = 0
+i = 0
 
 while True:
     sleep(1.0)
     system('cls')
     identity = input("Enter your ID number:")
-    acc = Account(identity, 0, date.today())
+    acc = Account(identity, 0, date.today(), i)
+    acc.setAccount()
     if idCheck(identity):
-        # accNum = random.choice(savings)
         menu()
         option = eval(input("Choose an option:"))
         while option != 4:
@@ -87,7 +99,7 @@ while True:
             elif option == 2:
                 out = eval(input("Enter the amount to withdraw: "))
                 acc.withdraw(out)
-                print("Money out:-R"+str(out))
+                print("Money out:-R" + str(out))
             elif option == 3:
                 dep = eval(input("Enter the amount to deposit:"))
                 acc.deposit(dep)
@@ -98,3 +110,4 @@ while True:
             option = eval(input("Choose an option:"))
     else:
         print("Please enter a valid ID number!!!.")
+    i += 1
